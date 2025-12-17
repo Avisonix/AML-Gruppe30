fc8507293193f68eab67fc3f45afce5381e98c60
IdaHustedDavidsen <idda22ab@student.cbs.dk>
2025-12-17 12:40:10 +0100

    cleaning of files

--- data/processed/README.txt
+++
@@ -1,9 +0,0 @@
-Denne mappe indeholder de data vi har processed som er klar til at blive brugt
-
-Typisk kommer filerne herfra fra preprocess.py 
-Vi overskriver gerne filer her, når vi laver nye versioner af rensede data.
-
-Eksempler:
-- data/processed/clean_data.csv
-- data/processed/features_ready.csv
-
--- tests/test_smoke.py
+++
@@ -1,6 +0,0 @@
-# Denne fil bruges til at teste, at projektet fungerer som forventet.
-# 'pytest' leder automatisk efter filer, der starter med "test_".
-#
-# En "smoke test" er en meget simpel test, som bare tjekker,
-# at projektet kan køre uden at crashe.
-# Men vi kan lave alle mulige andre test eller lignede som vi synes er relevant
